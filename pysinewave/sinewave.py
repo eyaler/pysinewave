a154451d631c1c43103b1602638d9b7ba90ff43d
eyaler <eyalgruss@gmail.com>
2021-08-22 01:43:16 +0300

    custom waveform

--- pysinewave/sinewave.py
+++ pysinewave/sinewave.py
@@ -11,12 +11,12 @@ class SineWave:
         and amplitude (volume).'''
 
     def __init__(self, pitch=0, pitch_per_second=12, decibels=0, decibels_per_second=1, channels=1, channel_side="lr",
-                samplerate=utilities.DEFAULT_SAMPLE_RATE):
+                samplerate=utilities.DEFAULT_SAMPLE_RATE, waveform=np.sin):
 
         self.sinewave_generator = sinewave_generator.SineWaveGenerator(
                                     pitch=pitch, pitch_per_second=pitch_per_second,
-                                    decibels = decibels, decibels_per_second=decibels_per_second,
-                                    samplerate=samplerate)
+                                    decibels=decibels, decibels_per_second=decibels_per_second,
+                                    samplerate=samplerate, waveform=waveform)
 
         # Create the output stream
         self.output_stream = sd.OutputStream(channels=channels, callback= lambda *args: self._callback(*args), 
